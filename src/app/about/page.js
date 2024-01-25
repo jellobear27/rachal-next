6a171f85fbd28196ca9c04925007681980a0d998
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2024-01-25 13:43:56 -0500

    created contact form

--- src/app/about/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>About content goes here!</div>
-  )
-}
-
-export default page
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function page() {
+
+
+
   return (
-    <div>This is My contact Page</div>
-  )
+    <div className="text-[#2a5f87]">
+      <form className="flex flex-col [&>*]:mt-3 [&>*]:border [&>*]:p-4 w-full" >
+        <div className="font-medium text-3xl">Please complete entire form</div>
+        <input type="text" placeholder="first name here.." />
+        <input type="text" placeholder="last name here.." />
+        <input type="email" placeholder="email  here.." />
+        <input type="phone" placeholder="phone  here.." />
+        <select>
+          <option value="" disabled selected>
+            Preferred Contact Method
+          </option>
+          <option value={"zoom"}>Zoom</option>
+          <option value={"phone"}>Phone</option>
+        </select>
+        <button type="submit" className="bg-slate-600 text-white">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default page
+export default page;
