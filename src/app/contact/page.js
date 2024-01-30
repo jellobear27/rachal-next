4d33c85c3e3b7bc56e8a56e9b93b016c56b41322
AJ DeMarco <79189312+ajdemarco95@users.noreply.github.com>
2024-01-29 19:44:20 -0500

    Added recaptcha provider

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useReCaptcha } from "next-recaptcha-v3";
+import { ReCaptchaProvider } from "next-recaptcha-v3";
 
 function page() {
   const [isLoading, setIsLoading] = useState(false);
@@ -43,42 +44,52 @@ function page() {
   };
 
   return (
-    <div className="text-[#2a5f87]">
-      <form className="flex flex-col [&>*]:mt-3 [&>*]:border [&>*]:p-4 w-full" onSubmit={handleSubmit}>
-        <div className="font-medium text-3xl">Please complete entire form</div>
-        <input name="firstName" type="text" placeholder="first name here.." />
-        <input name="lastName" type="text" placeholder="last name here.." />
-        <input name="email" type="email" placeholder="email  here.." />
-        <input name="phone" type="phone" placeholder="phone  here.." />
-        <select name="contactMethod">
-          <option value="" disabled selected>
-            Preferred Contact Method
-          </option>
-          <option value={"zoom"}>Zoom</option>
-          <option value={"phone"}>Phone</option>
-        </select>
-        <input
-          type="text"
-          name="firstNameLol"
-          placeholder="first name please"
-          className=" w-2 h-0 bg-[#f5f7f9] outline-transparent border-none absolute placeholder:text-[#f5f7f9] cursor-default -z-10"
-        />
-        <button type="submit" className="bg-slate-600 text-white" disabled={isLoading || requestSuccess}>
-          Submit
-        </button>
-        {requestSuccess && (
-        <div className="mt-12 text-2xl text-[green]">
-          Submission received! Thank you!
-        </div>
-      )}
-      {requestFailed && (
-        <div className="mt-12 text-2xl text-[red]">
-          Sorry, there's been an error... Try again later.
-        </div>
-      )}
-
-      </form>
-    </div>
+    <ReCaptchaProvider reCaptchaKey={`${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}>
+      <div className="text-[#2a5f87]">
+        <form
+          className="flex flex-col [&>*]:mt-3 [&>*]:border [&>*]:p-4 w-full"
+          onSubmit={handleSubmit}
+        >
+          <div className="font-medium text-3xl">
+            Please complete entire form
+          </div>
+          <input name="firstName" type="text" placeholder="first name here.." />
+          <input name="lastName" type="text" placeholder="last name here.." />
+          <input name="email" type="email" placeholder="email  here.." />
+          <input name="phone" type="phone" placeholder="phone  here.." />
+          <select name="contactMethod">
+            <option value="" disabled selected>
+              Preferred Contact Method
+            </option>
+            <option value={"zoom"}>Zoom</option>
+            <option value={"phone"}>Phone</option>
+          </select>
+          <input
+            type="text"
+            name="firstNameLol"
+            placeholder="first name please"
+            className=" w-2 h-0 bg-[#f5f7f9] outline-transparent border-none absolute placeholder:text-[#f5f7f9] cursor-default -z-10"
+          />
+          <button
+            type="submit"
+            className="bg-slate-600 text-white"
+            disabled={isLoading || requestSuccess}
+          >
+            Submit
+          </button>
+          {requestSuccess && (
+            <div className="mt-12 text-2xl text-[green]">
+              Submission received! Thank you!
+            </div>
+          )}
+          {requestFailed && (
+            <div className="mt-12 text-2xl text-[red]">
+              Sorry, there's been an error... Try again later.
+            </div>
+          )}
+        </form>
+      </div>
+    </ReCaptchaProvider>
   );
 }
 
