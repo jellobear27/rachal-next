aaa031b8bd0024162f215c596871ecee6814bcd2
AJ DeMarco <79189312+ajdemarco95@users.noreply.github.com>
2024-01-29 19:51:13 -0500

    Refactored

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,94 +1,13 @@
 "use client";
 
-import { useState } from "react";
-import { useReCaptcha } from "next-recaptcha-v3";
 import { ReCaptchaProvider } from "next-recaptcha-v3";
+import ContactForm from "@/components/ContactForm";
 
 function page() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [requestFailed, setRequestFailed] = useState(false);
-  const [requestSuccess, setRequestSuccess] = useState(false);
-
-  const { executeRecaptcha } = useReCaptcha();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    setRequestFailed(false);
-    setRequestSuccess(false);
-
-    const data = new FormData(e.target);
-
-    const token = await executeRecaptcha("form_submit");
-    data.append("g-recaptcha-response", token);
-
-    if (token) {
-      try {
-        const response = await fetch("/submit/contact", {
-          method: "POST",
-          body: data,
-        });
-
-        if (response.ok) {
-          setRequestSuccess(true);
-        } else {
-          setRequestFailed(true);
-        }
-      } catch (error) {
-        console.error(error);
-        setRequestFailed(true);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-  };
 
   return (
     <ReCaptchaProvider reCaptchaKey={`${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}>
-      <div className="text-[#2a5f87]">
-        <form
-          className="flex flex-col [&>*]:mt-3 [&>*]:border [&>*]:p-4 w-full"
-          onSubmit={handleSubmit}
-        >
-          <div className="font-medium text-3xl">
-            Please complete entire form
-          </div>
-          <input name="firstName" type="text" placeholder="first name here.." />
-          <input name="lastName" type="text" placeholder="last name here.." />
-          <input name="email" type="email" placeholder="email  here.." />
-          <input name="phone" type="phone" placeholder="phone  here.." />
-          <select name="contactMethod">
-            <option value="" disabled selected>
-              Preferred Contact Method
-            </option>
-            <option value={"zoom"}>Zoom</option>
-            <option value={"phone"}>Phone</option>
-          </select>
-          <input
-            type="text"
-            name="firstNameLol"
-            placeholder="first name please"
-            className=" w-2 h-0 bg-[#f5f7f9] outline-transparent border-none absolute placeholder:text-[#f5f7f9] cursor-default -z-10"
-          />
-          <button
-            type="submit"
-            className="bg-slate-600 text-white"
-            disabled={isLoading || requestSuccess}
-          >
-            Submit
-          </button>
-          {requestSuccess && (
-            <div className="mt-12 text-2xl text-[green]">
-              Submission received! Thank you!
-            </div>
-          )}
-          {requestFailed && (
-            <div className="mt-12 text-2xl text-[red]">
-              Sorry, there's been an error... Try again later.
-            </div>
-          )}
-        </form>
-      </div>
+      <ContactForm />
     </ReCaptchaProvider>
   );
 }
