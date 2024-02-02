f52543caca8edbe16de40caa624cdfcf7bb4036b
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2024-02-02 13:08:56 -0500

    Update MenuOverlay.js

--- src/components/MenuOverlay.js
+++ src/components/MenuOverlay.js
@@ -5,11 +5,11 @@ function MenuOverlay({ menuList, onMenuClick }) {
   return (
     <div className='absolute text-center left-0 min-h-full backdrop-blur-lg w-full mt-7'>
         {menuList.map((item, index) => (
-            <Link href={item.link} key={index}>
+            <a href={item.link} key={index}>
                 <h1 onClick={onMenuClick} className='text-[#2a5f87] font-bold text-[24px] mb-6 justify-center mt-10 hover:scale-110 transition-all ease-in-out duration-300 cursor-pointer'>
                     {item.title}
                 </h1>
-            </Link>
+            </a>
         ))}
     </div>
   );
