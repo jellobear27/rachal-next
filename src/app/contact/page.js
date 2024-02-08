0ff504d7853abee9f98c2f7fb7103483b3d18bfa
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2024-02-07 19:37:41 -0500

    idea

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -6,7 +6,7 @@ import ContactForm from "@/components/ContactForm";
 function page() {
 
   return (
-    <ReCaptchaProvider reCaptchaKey={`${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}>
+    <ReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}>
       <ContactForm />
     </ReCaptchaProvider>
   );
